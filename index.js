abb3aaf2397559ff0b78f7a081ca9a719972e8e5
Shazzadhossensunny <shazzadhossensunny@gmail.com>
2024-01-28 16:29:04 +0600

    condition

---
+++ index.js
@@ -0,0 +1,138 @@
+// task free drinks
+/*const burger = 550;
+if (burger > 500) {
+    console.log("you get free cock");
+
+}
+else{
+    console.log("pay for coke 30tk");
+}*/
+
+// *********************************************************************
+
+// bmi calculator
+
+/*const weight = 55;
+const height = 5.1 * 0.3048;  // covert feet to meter 1 feet = 0.3048 meter
+
+const BMI = weight / (height ** 2);
+if(BMI < 18.5){
+    console.log("you are underweight");
+
+}
+else if(BMI >= 18.5 && BMI <= 24.9 ){
+    console.log("you are normal");
+
+}
+else if(BMI >= 25 && BMI <= 29.9 ){
+    console.log("you are overweight");
+
+}
+else{
+    console.log("otherwise you are obese");
+}*/
+// *********************************************************************
+// grade calculator
+
+/*const marks = 10;
+let grade;
+
+if (marks <= -1 || marks >=101) {
+    console.log("write correct number");
+
+}
+else if(marks >= 90 && marks <= 100 ){
+    grade = "A";
+
+}
+else if(marks >= 80 && marks <= 89 ){
+    grade = "B";
+
+}
+else if(marks >= 70 && marks <= 79 ){
+    grade = "C";
+
+}
+else if(marks >= 60 && marks <= 69 ){
+    grade = "D"
+
+}
+else if(marks >= 0 && marks <= 59 ){
+    grade = "F"
+
+}
+console.log(grade);
+
+// *********************************************************************
+
+// nested friend
+
+/*const myScore = 85;
+const friendScore = 39;
+
+if (myScore > 80) {
+    if(friendScore >= 80){
+        console.log("go for lunch");
+    }
+    if(friendScore < 80 && friendScore >= 60 ){
+        console.log("good luck next time");
+
+    }
+    if(friendScore < 60 && friendScore >= 40 ){
+        console.log("keep your friend message unseen");
+
+    }
+    if(friendScore < 40){
+        console.log("block your friend");
+
+    }
+
+}
+else {
+    console.log("go to home and sleep and act sad!")
+}*/
+// *********************************************************************
+// ternary number
+/*const num1 = 84;
+const num2 = 67;
+let result;
+if( num1 > num2){
+    result =num1 * 2;
+
+}
+else{
+    result =num1 + num2;
+}
+
+
+const result = num1 > num2 ? num1 * 2 : num1 + num2;
+console.log(result);*/
+
+// *********************************************************************
+
+// bus fare calculator
+  /*const age = 60;
+  const price = 800;
+
+
+  if (age < 10 ) {
+    console.log("no ticket need");
+
+  }
+  else if( age >= 10 && age <= 30){
+    // students get 50% discount
+      var discount = price * 50 / 100;
+      var payNeed = price - discount;
+      console.log(payNeed);
+
+  }
+  else if( age >= 60){
+    // senior citizen get 15% discount
+    var discount = price * 15 / 100;
+    var payNeed = price - discount;
+    console.log(payNeed);
+
+  }
+  else {
+    console.log("regular ticket fare 800 tk");
+  }*/
